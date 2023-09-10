81ef4c0eb356465a3827f762440fac78e9aa2fd6
Sevgul KETENCI <ketenci366660@gmail.com>
2023-09-10 22:54:25 +0300

    Proje dosyası ekle.

---
+++ PorfolioProject/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PorfolioProject.Models;
+using System.Diagnostics;
+
+namespace PorfolioProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
